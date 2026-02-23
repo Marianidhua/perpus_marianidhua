961e68b5a5dd0791afe0720227657241ef7893ce
tiangterbang6 <222720536+tiangterbang6@users.noreply.github.com>
2026-02-23 09:25:05 +0800

    perpus_marianidhua

---
+++ koneksi.php
@@ -0,0 +1,12 @@
+<?php 
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+$koneksi = mysqli_connect("localhost", "root", "", "perpus_marianidhua");
+
+// Check connection
+if (mysqli_connect_errno()) {
+    echo "failed to connect to MySQL: " . mysqli_connect_error();
+}
+?>
